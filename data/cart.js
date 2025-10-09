9ad908acfa008a2bfbd5c30a642c4175d053eea6
learn0246 <learn0246@gmail.com>
2025-10-09 17:29:02 +0800

    Save data and generate HTML for delivery options

--- data/cart.js
+++ data/cart.js
@@ -3,9 +3,11 @@ export let cart =
   [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2,
+    deliveryOptionId: '1'
   },{
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1
+    quantity: 1,
+    deliveryOptionId: '2'
   }];
 
 export function addToCart(productId){
@@ -23,7 +25,8 @@ export function addToCart(productId){
   }else{
     cart.push({
       productId,
-      quantity: selectorQuantity
+      quantity: selectorQuantity,
+      deliveryOptionId: '1'
     })
   }
 
