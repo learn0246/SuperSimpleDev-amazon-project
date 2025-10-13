02e5006df8baa600448540d8e568da936ecff0f9
learn0246 <learn0246@gmail.com>
2025-10-13 20:58:14 +0800

    Create oop version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderChedckoutHeader } from "./checkout/checkoutHeader.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import '../data/cart-oop.js';
 
 renderPaymentSummary();
 renderOrderSummary();
